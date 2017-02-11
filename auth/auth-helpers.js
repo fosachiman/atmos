8c1cb52452a445d0c3f537a177426fa3fc221f73
fosachiman <jasonphumphreys@gmail.com>
2017-02-11 13:45:42 -0500

    added auth routes and helper functions

--- auth/auth-helpers.js
+++ auth/auth-helpers.js
@@ -8,3 +8,21 @@ const models = require('../db/models/index');
 function comparePass(userPassword, databasePassword) {
   return bcrypt.compareSync(userPassword, databasePassword);
 }
+
+//redirects user to user page if they're already logged in
+function loginRedirect(req, res, next) {
+  if (req.user) res.redirect('/user');
+
+  return next();
+}
+
+//creates user in the database, helper function from register route
+function createUser(req, res) {
+  const salt = bcrypt.genSaltSync();
+  const hash = bcrypt.hashSync(req.body.password, salt);
+  return models.User.create({
+    password: hash,
+    name: req.body.name,
+    email: req.body.email,
+  });
+}
---
+++ routes/auth.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const router = express.Router();
+
+const authHelpers = require('../auth/auth-helpers');
+const passport = require('../auth/local');
+
+
+//sends user to register page, unless they're already logged in
+router.get('/register', authHelpers.loginRedirect, (req, res)=> {
+  res.render('auth/register');
+});
+
+//sends registration data to the db and redirects to user page
+router.post('/register', (req, res, next)  => {
+  authHelpers.createUser(req, res)
+  .then((user) => {
+    req.login(user, (err) => {
+      if (err) return next(err);
+
+      res.redirect('/user');
+    });
+  })
+  .catch((err) => { res.status(500).json({ status: 'error' }); });
+});
+
+//sends user to the login page
+router.get('/login', authHelpers.loginRedirect, (req, res)=> {
+  res.render('auth/login');
+});
+
+//redirects user based on successful or failed login
+router.post('/login', passport.authenticate('local', {
+    successRedirect: '/user',
+    failureRedirect: '/auth/login',
+    failureFlash: true
+  })
+);
+
+router.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
+
+module.exports = router;
