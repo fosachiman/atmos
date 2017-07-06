6b1a1dd9e36922e463a34d33699d4a508004566b
fosachiman <jasonphumphreys@gmail.com>
2017-07-06 12:06:48 -0400

    skycon script in one file

--- public/javascripts/skycon_script.js
+++ public/javascripts/skycon_script.js
@@ -1,18 +1,21 @@
 
-let skycons = new Skycons({
-  "color": "white"
-  // "resizeClear": true
-});
+// let skycons = new Skycons({
+//   "color": "white"
+//   // "resizeClear": true
+// });
 
-// find element on the dom and set it to equal the icon value
-// that represents the present weather in the ejs file
-let iconElement = document.querySelectorAll('.icon');
-iconElement.forEach(function(element) {
-  let icon = element.getAttribute('icon');
-  let id = element.getAttribute('id')
-  skycons.add(id, icon);
-})
+// // find element on the dom and set it to equal the icon value
+// // that represents the present weather in the ejs file
+// let iconElement = document.querySelectorAll('.icon');
 
 
-// start animation!
-skycons.play();
+// iconElement.forEach(function(element) {
+//   let icon = element.getAttribute('icon');
+//   let id = element.getAttribute('id');
+//   skycons.add(id, icon);
+// })
+
+
+// // start animation!
+// skycons.play();
+
--- public/javascripts/skycons.js
+++ public/javascripts/skycons.js
@@ -706,3 +706,24 @@
 
   global.Skycons = Skycons;
 }(this));
+
+
+let skycons = new Skycons({
+  "color": "white"
+  // "resizeClear": true
+});
+
+// find element on the dom and set it to equal the icon value
+// that represents the present weather in the ejs file
+let iconElement = document.querySelectorAll('.icon');
+
+
+iconElement.forEach(function(element) {
+  let icon = element.getAttribute('icon');
+  let id = element.getAttribute('id');
+  skycons.add(id, icon);
+})
+
+
+// start animation!
+skycons.play();
